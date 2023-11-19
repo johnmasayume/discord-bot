544131c5462a1980e750cc45bce29a878569491e
John M1-Air <test@gmail.com>
2023-11-19 19:10:05 +0800

    updated birthday code to support category of discord and personal

--- bot/rem/extensions/birthday.py
+++ bot/rem/extensions/birthday.py
@@ -41,23 +41,47 @@ class BirthdayCog(commands.Cog):
 
             # Specify the channel ID where you want to send birthday messages
             target_channel_id = config.PRIVATE_CHANNEL_ID
-
             target_channel = self.bot.get_channel(target_channel_id)
-
             if not target_channel:
                 print(f"Invalid channel ID: {target_channel_id}")
                 return
 
-            for user_id, birthday in birthday_data.items():
-                birthday_date = datetime.strptime(birthday, "%Y-%m-%d")
-                if now.day == birthday_date.day and now.month == birthday_date.month:
-                    member = await self.bot.fetch_user(int(user_id))
-                    if member:
-                        print(f"Sending birthday message to {member.display_name} in {target_channel.name}")
-                        await target_channel.send(f"Happy Birthday, {member.mention}!")
+            # Check if today is a birthday
+            today_date = now.strftime("%Y-%m-%d")
+            if today_date in birthday_data:
+                today_birthdays = birthday_data[today_date]
+
+                # Check "discord" category
+                if "discord" in today_birthdays:
+                    discord_member_ids = today_birthdays["discord"]
+                    for user_id in discord_member_ids:
+                        member = await self.bot.fetch_user(user_id)
+                        if member:
+                            print(f"Sending Discord birthday message to {member.display_name} in {target_channel.name}")
+                            await target_channel.send(f"Happy Discord Birthday, {member.mention}!")
+
+                # Check "personal" category
+                if "personal" in today_birthdays:
+                    personal_data = today_birthdays["personal"]
+                    if "name" in personal_data:
+                        names = personal_data["name"]
+                        owner_id = config.OWNER_ID
+
+                        # Tag the owner and send names
+                        owner_mention = f"<@{owner_id}>"
+                        names_str = ', '.join(names)
+                        personal_channel_id = config.PRIVATE_CHANNEL_ID
+                        personal_channel = self.bot.get_channel(personal_channel_id)
+                        if personal_channel:
+                            print(f"Sending Personal birthday message to {owner_mention} in {personal_channel.name}")
+                            await personal_channel.send(f"Reminder, it's the birthday of {owner_mention}'s friends: {names_str}")
 
             # Wait for the next day
             await asyncio.sleep(60 * 24)  # 24 hours in minutes
+
+    async def save_birthday_data(self, data):
+        with open(self.get_birthday_data_path(), 'w') as file:
+            json.dump(data, file)
 
 async def setup(bot):
     await bot.add_cog(BirthdayCog(bot))
