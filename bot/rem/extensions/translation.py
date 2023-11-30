1d1e617f37f46010ffc5ff9434b906e7a117b075
John <john.lim@keysight.com>
2023-11-30 18:03:17 +0800

    revert

--- bot/rem/extensions/translation.py
+++ bot/rem/extensions/translation.py
@@ -24,9 +24,22 @@ class Translation(commands.Cog):
         # Check if the message is in the desired channel
         target_channel_id = config.GENERAL_CHANNEL_ID  # Replace with the actual channel ID
         if message.channel.id == target_channel_id:
-            # Translate the entire message to English
-            translated_text = self.translator.translate(message.content, src='auto', dest='en').text
-            await message.channel.send(f'Original: {message.content}\nTranslated: {translated_text}')
+            # Extract non-English characters from the message
+            non_english_chars = [char for char in message.content if ord(char) > 127]
+
+            # Check if there are Japanese or Chinese characters
+            jp_chars = [char for char in non_english_chars if '\u3040' <= char <= '\u30ff']
+            cn_chars = [char for char in non_english_chars if '\u4e00' <= char <= '\u9fff']
+
+            if jp_chars or cn_chars:
+                # Translate the characters to English
+                translated_text = ''
+                if jp_chars:
+                    translated_text += self.translator.translate(''.join(jp_chars), dest='en').text
+                if cn_chars:
+                    translated_text += self.translator.translate(''.join(cn_chars), dest='en').text
+
+                await message.channel.send(f'Original: {message.content}\nTranslated: {translated_text}')
 
 
 async def setup(bot: commands.Bot) -> None:
