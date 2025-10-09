3c20c830d6b74c796ef307991f7d0d24e02d5350
Masayume <masayume@example.com>
2025-10-09 20:02:58 +0800

    add version checking for bot and change jpy wise target rate to 0.026 insted of 0.027

--- bot/rem/main.py
+++ bot/rem/main.py
@@ -1,7 +1,7 @@
 from discord.ext import commands
 import discord
 import config
-
+from util import log_to_discord_channel
 
 INTENTS = discord.Intents.default()
 INTENTS.message_content = True
@@ -14,6 +14,12 @@ bot = commands.Bot(
 @bot.event
 # async def setup_hook() -> None:
 async def on_ready():
+    version_id = config.BOT_VERSION
+    version_log_channel_id = config.LOG_CHANNEL_ID_VERSION
+    log_category_id = config.LOG_CHANNEL_CATEGORY_ID
+    
+    category = bot.get_channel(log_category_id)
+    await log_to_discord_channel(category, f"Rem version {version_id}", version_log_channel_id)
     for extension in config.EXTENSIONS:
         await bot.load_extension(config.EXTENSIONS_DIR+extension)
 
